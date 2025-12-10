ed7c6fdd1e121a8aa04d5d3dfd4a0f34f7ce931e
NullByteM <mostafa.cybersec@gmail.com>
2025-12-10 20:18:44 +0200

    Update fonts.nix

--- home-manager/config/fonts.nix
+++ home-manager/config/fonts.nix
@@ -2,8 +2,8 @@
   fonts.fontconfig = {
     enable = true;
     defaultFonts = {
-      serif = [ "Roboto Serif" "Noto Naskh Arabic" "Noto Serif CJK" ];
-      sansSerif = [ "Roboto" "Noto Naskh Arabic" "Noto Sans CJK" ];
+      serif = [ "Roboto Serif" "IBM Plex Sans Arabic" "Noto Serif CJK" ];
+      sansSerif = [ "Roboto" "IBM Plex Sans Arabic" "Noto Sans CJK" ];
       monospace = [ "JetBrains Mono" ];
       emoji = [ "Noto Color Emoji" "Twemoji" "Segoe UI Emoji" "Apple Color Emoji" ];
     };
